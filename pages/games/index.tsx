71c22aa29e99570605b8cadd0015ad9818fe5b0f
ccp92 <mail@chrisparsons.dev>
2022-06-13 00:09:37 +0100

    Add dummy getStaticProps function to Games page

--- pages/games/index.tsx
+++ pages/games/index.tsx
@@ -1,11 +1,27 @@
 import { NextPage } from "next";
+import { ReactNode } from "react";
 
-const Games: NextPage = () => {
+const Games: NextPage = (props: {
+  game?: { name: string };
+  children?: ReactNode;
+}) => {
+  const { game } = props;
   return (
     <div>
       <h1>Games</h1>
+      <h2>{game?.name}</h2>
     </div>
   );
 };
+
+export async function getStaticProps() {
+  return {
+    props: {
+      game: {
+        name: "Tetris",
+      },
+    },
+  };
+}
 
 export default Games;
