8777664e43f53872bb5a72ce75d61ea977e873d7
ccp92 <mail@chrisparsons.dev>
2022-10-17 21:23:48 +0100

    Add LoginButton skeleton

---
+++ components/LoginButton/index.test.tsx
@@ -0,0 +1,10 @@
+import { render, screen } from "@testing-library/react";
+
+import LoginButton from "../LoginButton";
+
+describe("LoginButton", () => {
+  it("renders a login button", () => {
+    render(<LoginButton />);
+    expect(screen.getByText("Login")).toBeTruthy();
+  });
+});
